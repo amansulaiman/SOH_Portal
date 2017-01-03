787cd1b74c90d715e38ea47beae0f937651fb495
Abdulrahman Sulaiman Iliyasu <amansulaiman92@gmail.com>
2017-01-03 06:33:04 -0800

    UI Changes

--- Grading_SOH_Final_Rel/App_Start/BundleConfig.cs
+++ Grading_SOH_Final_Rel/App_Start/BundleConfig.cs
@@ -8,14 +8,18 @@ namespace Grading_SOH_Final_Rel
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            //Enable Bundle Optimation
+            BundleTable.EnableOptimizations = true;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.1.1.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -27,13 +31,6 @@ namespace Grading_SOH_Final_Rel
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/ejscripts").Include(
-                           "~/Scripts/jsrender.min.js",
-                           "~/Scripts/jquery.easing-1.3.min.js",
-                            "~/Scripts/ej/ej.web.all.min.js",
-                            "~/Scripts/ej/ej.unobtrusive.min.js"));
-            bundles.Add(new StyleBundle("~/bundles/ejstyles").Include(
-                      "~/ejThemes/flat-saffron/ej.web.all.min.css"));
 
             bundles.Add(new StyleBundle("~/Bundles/css")
                 .Include("~/Content/css/bootstrap.css")
@@ -53,6 +50,24 @@ namespace Grading_SOH_Final_Rel
                 .Include("~/Content/js/plugins/icheck/icheck.js")
                 .Include("~/Content/js/app.js")
                 .Include("~/Content/js/init.js"));
+
+            bundles.Add(new ScriptBundle("~/Bundles/ejgrid")
+                .Include("~/Scripts/ej/ej.widget.grid.min.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/Bundles/ejchart")
+                .Include("~/Scripts/ej/ej.widget.chart.min.js")
+                );
+
+            bundles.Add(new StyleBundle("~/bundles/ejstyles")
+                .Include("~/ejThemes/bootstrap-theme/ej.theme.min.css", "~/ejThemes/ej.widgets.core.bootstrap.min.css"));
+
+            
+            bundles.Add(new ScriptBundle("~/bundles/ejscripts").Include(
+                           "~/Scripts/jsrender.min.js",
+                           "~/Scripts/jquery.easing-1.3.min.js",
+                            "~/Scripts/ej/ej.unobtrusive.min.js"));
+
         }
     }
 }
